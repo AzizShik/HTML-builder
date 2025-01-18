f2e86ca6d873eba5941de7b1e93c450af5364fd9
Abdyl-Aziz <azizshihabov@mail.ru>
2025-01-18 16:59:07 +0300

    feat: add solution for 01-read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,2 +1,7 @@
 const path = require('path');
+const fs = require('fs');
+const { stdout } = require('process');
 
+const input = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+input.on('data', (data) => stdout.write(data));
